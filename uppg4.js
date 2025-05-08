7b9e1f435a57d935adfe11dcf0af3492b2a297b4
sebastian@temtron.se <sebastian@temtron.se>
2025-05-08 13:53:24 +0200

    Print persons age

--- uppg4.js
+++ uppg4.js
@@ -6,6 +6,12 @@ function uppg4() {
     // tilldela age 25
     // skriv ut med dotnotation åldern i konsolen
 
+    var person = {
+        name: "Sebastian Enerstrand",
+        age: 25
+    };
+    
+    console.log(person.age);
 
 }
 
