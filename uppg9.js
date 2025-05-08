cbefd44e22522ca88b7a61830841cdf9a6c37859
sebastian@temtron.se <sebastian@temtron.se>
2025-05-08 14:07:32 +0200

    Move odd/even to new row

--- uppg9.js
+++ uppg9.js
@@ -8,17 +8,21 @@ function uppg9 (){
 
     // anropa funktionen och skicka med en array som argument
 
-  
+  var numbers = [5, 12, 7, 10, 20, 9, 11];
+    sort(numbers);
     
     function sort(array) {
         for (let i = 0; i < array.length; i++) {
             if (array[i] % 2 === 0) {
-                console.log(array[i] + " jämt");
+                console.log(array[i]);
+                console.log("jämt");
             } else {
-                console.log(array[i] + " udda");
+                console.log(array[i]);
+                console.log("udda");
             }
         }
     }
 }
 
+uppg9();
 module.exports = { uppg9 };
